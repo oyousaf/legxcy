7d0f98c4ddb35f2f8ce49c9fb855ba90f38ae805
Omar 🍉 <o_yousaf@live.co.uk>
2025-06-03 16:11:16 +0100

    squashing bugs

--- backend/lib/supabase.js
+++ backend/lib/supabase.js
@@ -4,6 +4,6 @@ dotenv.config();
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
