0506cf0a8264b1038d2ed80530be91291dfb58ce
Omar 🍉 <o_yousaf@live.co.uk>
2025-07-28 14:43:24 +0100

    squashing bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import { useCartStore } from "./stores/useCartStore";
 import PurchaseSuccessPage from "./pages/PurchaseSuccessPage";
 import PurchaseCancelPage from "./pages/PurchaseCancelPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const { user, checkAuth } = useUserStore();
